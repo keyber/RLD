846d07734b682154898b8e86f655914acf83dafe
keyber <keyvan.beroukhim@etu.upmc.fr>
2019-09-30 13:21:58 +0200

    valueIteration implementé. bugfix: curr et new étaient le meme objet.
    todo: essayer sur d'autres jeux/instances

--- TME2/mdp_connu.py
+++ TME2/mdp_connu.py
@@ -19,11 +19,8 @@ class RandomAgent:
 
 class PolicyIterationAgent:
     def __init__(self, env, state_space, action_space, p, obs_to_states, gamma=.99, cst=-.1):
-        # initialisation de la politique aléatoirement
-        self.policy = {state: action_space.sample() for state in state_space}
-
-        # initialisation des valeurs aléatoirement
-        self.state_values = {state: 0 for state in state_space}
+        self.policy = None
+        self.state_values = None
         self.action_space = action_space
         self.state_space = state_space
         self.p = p
@@ -32,14 +29,13 @@ class PolicyIterationAgent:
         self.obs_to_states = obs_to_states
         self.env = env
     
-    def get_state_values_from_policy(self, eps=1e-3):
+    def _get_state_values_from_policy(self, eps):
         current_state_values = self.state_values
         new_state_values = {}
         
         diff = eps
         while diff >= eps:
-            diff = 0
-
+            
             for state in self.state_space:
                 state_value = self.cst
                 
@@ -48,23 +44,26 @@ class PolicyIterationAgent:
                     if not done:
                         val += self.gamma * current_state_values[state2]
                     val *= proba
-
-                    state_value +=  val
+                    
+                    state_value += val
                 
-                diff += (state_value - current_state_values[state]) ** 2
                 new_state_values[state] = state_value
+                assert current_state_values is not new_state_values
             
+            diff = 0
+            for state in self.state_space:
+                diff += (new_state_values[state] - current_state_values[state]) ** 2
+                current_state_values[state] = new_state_values[state]
             diff = np.sqrt(diff)
-            current_state_values = new_state_values
-
+        
         return new_state_values
     
-    def update_policy(self):
+    def _update_policy(self):
         new_policy = {}
         for state in self.state_space:
             best_action = None
             max_value = -np.inf
-
+            
             for action in self.p[state].keys():
                 s = 0
                 for proba, state2, reward, done in self.p[state][action]:
@@ -73,23 +72,29 @@ class PolicyIterationAgent:
                         val += self.gamma * self.state_values[state2]
                     val *= proba
                     s += val
-
+                
                 if s > max_value:
-                    best_action = action 
-                    max_value = s 
-
+                    best_action = action
+                    max_value = s
+            
             new_policy[state] = best_action
         
         return new_policy
-
-    def compute_best_policy(self):
+    
+    def compute_best_policy(self, eps=1e-6):
+        # initialisation de la politique aléatoirement
+        self.policy = {state: self.action_space.sample() for state in self.state_space}
+    
+        # initialisation des valeurs aléatoirement
+        self.state_values = {state: 0 for state in self.state_space}
+    
         change = True
         while change:
             # on ne réinitialise pas V à chaque itération
-            self.state_values = self.get_state_values_from_policy()
-
-            new_policy = self.update_policy()
-
+            self.state_values = self._get_state_values_from_policy(eps)
+            
+            new_policy = self._update_policy()
+            
             change = False
             for state in self.policy.keys():
                 if self.policy[state] != new_policy[state]:
@@ -97,30 +102,112 @@ class PolicyIterationAgent:
                     break
             
             self.policy = new_policy
-
+    
     def act(self, observation, _reward, _done):
         current_state = self.env.state2str(observation)
         return self.policy[current_state]
 
 
-def main():
+class ValueIterationAgent:
+    def __init__(self, env, state_space, _action_space, p, _obs_to_states, gamma=.99, cst=-.1):
+        self.policy = None
+        self.state_values = None
+        self.state_space = state_space
+        self.p = p
+        self.gamma = gamma
+        self.cst = cst
+        self.env = env
+    
+    def _value_iteration(self, eps):
+        current_state_values = self.state_values
+        new_state_values = {}
+        diff = eps
+    
+        while diff >= eps:
+            for state in self.state_space:
+                state_value = -np.inf
+            
+                for action in self.p[state].keys():
+                    value = self.cst
+                    for proba, state2, reward, done in self.p[state][action]:
+                        val = reward
+                        if not done:
+                            val += self.gamma * current_state_values[state2]
+                        val *= proba
+                    
+                        value += val
+                
+                    state_value = max(state_value, value)
+            
+                new_state_values[state] = state_value
+        
+            diff = 0
+            for state in self.state_space:
+                diff += (new_state_values[state] - current_state_values[state]) ** 2
+                current_state_values[state] = new_state_values[state]
+            diff = np.sqrt(diff)
+            
+        return new_state_values
+
+    def _update_policy(self):
+        """meme code que policy_iteration"""
+        new_policy = {}
+        for state in self.state_space:
+            best_action = None
+            max_value = -np.inf
+        
+            for action in self.p[state].keys():
+                s = self.cst
+                for proba, state2, reward, done in self.p[state][action]:
+                    val = reward
+                    if not done:
+                        val += self.gamma * self.state_values[state2]
+                    val *= proba
+                    s += val
+            
+                if s > max_value:
+                    best_action = action
+                    max_value = s
+        
+            new_policy[state] = best_action
+    
+        return new_policy
+
+    def compute_best_policy(self, eps=1e-6):
+        # initialisation des valeurs aléatoirement
+        self.state_values = {state: 0 for state in self.state_space}
+        
+        self.state_values = self._value_iteration(eps)
+        
+        self.policy = self._update_policy()
+        
+    
+    def act(self, observation, _reward, _done):
+        current_state = self.env.state2str(observation)
+        return self.policy[current_state]
+
+
+def _main_demo():
     env = gym.make("gridworld-v0")
     env.seed(0)  # Initialise le seed du pseudo-random
     print("actions possibles:", env.action_space)  # Quelles sont les actions possibles
     # print(env.step(1))  # faire action 1 et retourne l'observation, le reward, et un done un booleen (jeu fini ou pas)
     statedic, mdp = env.getMDP()  # recupere le mdp : statedic
-    print("Nombre d'etats:",len(statedic))  # nombre d'etats ,statedic : etat-> numero de l'etat
+    print("Nombre d'etats:", len(statedic))  # nombre d'etats ,statedic : etat-> numero de l'etat
     state, transitions = list(mdp.items())[0]
     print("ex state:", state)  # un etat du mdp
-    print("ex transistions:", transitions)  # dictionnaire des transitions pour l'etat :  {action-> [proba,etat,reward,done]}
+    print("ex transistions:",
+          transitions)  # dictionnaire des transitions pour l'etat :  {action-> [proba,etat,reward,done]}
     
     # Execution avec un Agent
-    agent = PolicyIterationAgent(env, mdp.keys(), env.action_space,
-                                mdp, env.getMDP()[0], gamma=1-1e-3, cst=-.01)
+    # agent = PolicyIterationAgent(env, mdp.keys(), env.action_space,
+    #                             mdp, env.getMDP()[0], gamma=1-1e-3, cst=-.01)
+    agent = ValueIterationAgent(env, mdp.keys(), env.action_space,
+                                mdp, env.getMDP()[0], gamma=1 - 1e-3, cst=-.01)
     agent.compute_best_policy()
     
     # env.render()  # permet de visualiser la grille du jeu
-    env.render(mode="human") #visualisation sur la console
+    env.render(mode="human")  #visualisation sur la console
     
     # Faire un fichier de log sur plusieurs scenarios
     outdir = 'gridworld-v0/random-agent-results'
@@ -130,7 +217,7 @@ def main():
     episode_count = 1000
     reward = 0
     done = False
-    FPS = 1e-6 # ~temps de pause entre deux affichages
+    FPS = 1e-6  # ~temps de pause entre deux affichages
     assert FPS > 0
     for i in range(episode_count):
         obs = envm.reset()
@@ -149,9 +236,51 @@ def main():
             if done:
                 print("Episode : " + str(i) + " rsum=" + str(rsum) + ", " + str(j) + " actions")
                 break
-
+    
     print("done")
     env.close()
 
+def _main_perf():
+    from time import time
+    env = gym.make("gridworld-v0")
+    env.seed()
+    statedic, mdp = env.getMDP()
+    
+    timeP = []
+    timeV = []
+    n = 3
+    list_gamma = [1 - 1e-1, 1 - 1e-3]
+    list_cst = [0, 1e-3, 1e-1]
+    for gamma in list_gamma:
+        for cst in list_cst:
+            for _ in range(3):
+                t0 = time()
+                agentP = PolicyIterationAgent(env, mdp.keys(), env.action_space,
+                                             mdp, env.getMDP()[0], gamma=gamma, cst=cst)
+                agentP.compute_best_policy(eps=1e-15)
+                timeP.append(time() - t0)
+                
+                t0 = time()
+                agentV = ValueIterationAgent(env, mdp.keys(), env.action_space,
+                                            mdp, env.getMDP()[0], gamma=gamma, cst=cst)
+                agentV.compute_best_policy(eps=1e-15)
+                timeV.append(time() - t0)
+                
+                assert agentP.policy.keys() == agentV.policy.keys()
+                for k in agentP.policy.keys():
+                    assert agentP.policy[k] == agentV.policy[k]
+    
+    print("tests passés")
+    print("timeP moy:", sum(timeP)/len(timeP), "max:", max(timeP))
+    print("timeV moy:", sum(timeV)/len(timeV), "max:", max(timeV))
+    
+    env.close()
+
+
+def main():
+    _main_demo()
+    _main_perf()
+    
+    
 if __name__ == '__main__':
     main()
