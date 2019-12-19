1bcbb4c204a1eb7e9cfc3904cc0b85f615fbe20d
keyber <keyvan.beroukhim@etu.upmc.fr>
2019-12-19 14:11:24 +0100

    séparation en act, get_reward, resest

--- c_continuous_space/DQN.py
+++ c_continuous_space/DQN.py
@@ -13,7 +13,7 @@ class DQN_Agent:
         self.eps_decay = eps_decay
         self.replay_memory_n = replay_memory_n
         self.gamma = gamma
-
+        
         self.action_space = action_space
         self.Q = Q
         self.Q_old = copy.deepcopy(self.Q)
@@ -30,65 +30,57 @@ class DQN_Agent:
         self.env = env
         self.with_exp_replay = with_exp_replay
         self.with_target_network = with_target_network
-        
     
     def _update(self, observation, reward, done):
-        s2 = torch.tensor(observation)
-        phi2 = self.phi(s2)
+        phi2 = self.phi(torch.tensor(observation))
         
-        # ne sauvegarde rien à la première action
-        if self.last_phi_state is not None:
-            if self.with_exp_replay:
-                # save
-                self.replay_memory[self.replay_memory_ind % len(self.replay_memory)] =(
-                    self.last_phi_state,
-                    torch.tensor([self.last_action]),
-                    torch.tensor([reward], dtype=torch.float),
-                    phi2,
-                    torch.tensor([done], dtype=torch.bool))
-                self.replay_memory_ind += 1
-
-                # sample for experience replay
-                els = np.random.choice(range(min(self.replay_memory_ind, len(self.replay_memory))),
-                                       min(self.replay_memory_ind, self.replay_memory_n))
-                l = self.replay_memory[els]
-                (lphi, la, lr, lphi2, ld) = map(torch.stack, zip(*l))
-                la = la.squeeze(1)
-                lr = lr.squeeze(1)
-                ld = ld.squeeze(1)
-            else:
-                lphi = self.last_phi_state.unsqueeze(0)
-                la = torch.tensor([self.last_action]).unsqueeze(0)
-                lr = torch.tensor([reward], dtype=torch.float).unsqueeze(0)
-                lphi2 = phi2.unsqueeze(0)
-                ld = torch.tensor([done], dtype=torch.bool).unsqueeze(0)
+        if self.with_exp_replay:
+            # save
+            self.replay_memory[self.replay_memory_ind % len(self.replay_memory)] = (
+                self.last_phi_state,
+                torch.tensor([self.last_action]),
+                torch.tensor([reward], dtype=torch.float),
+                phi2,
+                torch.tensor([done], dtype=torch.bool))
+            self.replay_memory_ind += 1
             
-            # gradient descent
-            self.optim.zero_grad()
-            phi2 = phi2.detach()
-            if self.with_target_network:
-                y_true = torch.where(ld, lr, lr + self.gamma * torch.max(self.Q_old(lphi2), dim=1)[0]) #max retourne (max, argmax)
-            else:
-                y_true = torch.where(ld, lr, lr + self.gamma * torch.max(self.Q(lphi2), dim=1)[0])  # max retourne (max, argmax)
-            y_pred = self.Q(lphi)[range(len(lphi)), la] # advanced indexing (différent de [:, la])
-            assert y_true.requires_grad == True
-            y_true = y_true.detach()
-            y_true.requires_grad = False
-            assert y_pred.requires_grad == True
-            loss = self.loss(y_pred, y_true)
-            loss.backward()
-            self.optim.step()
-
+            # sample for experience replay
+            els = np.random.choice(range(min(self.replay_memory_ind, len(self.replay_memory))),
+                                   min(self.replay_memory_ind, self.replay_memory_n))
+            l = self.replay_memory[els]
+            (lphi, la, lr, lphi2, ld) = map(torch.stack, zip(*l))
+            la = la.squeeze(1)
+            lr = lr.squeeze(1)
+            ld = ld.squeeze(1)
+        else:
+            lphi = self.last_phi_state.unsqueeze(0)
+            la = torch.tensor([self.last_action]).unsqueeze(0)
+            lr = torch.tensor([reward], dtype=torch.float).unsqueeze(0)
+            lphi2 = phi2.unsqueeze(0)
+            ld = torch.tensor([done], dtype=torch.bool).unsqueeze(0)
+        
+        # gradient descent
+        self.optim.zero_grad()
+        phi2 = phi2.detach()
+        if self.with_target_network:
+            y_true = torch.where(ld, lr, lr + self.gamma *
+                                 torch.max(self.Q_old(lphi2), dim=1)[0]) #max retourne (max, argmax)
+        else:
+            y_true = torch.where(ld, lr, lr + self.gamma *
+                                 torch.max(self.Q(lphi2), dim=1)[0])  # max retourne (max, argmax)
+        
+        y_pred = self.Q(lphi)[range(len(lphi)), la]  # advanced indexing (différent de [:, la])
+        assert y_true.requires_grad == True
+        y_true = y_true.detach()
+        y_true.requires_grad = False
+        assert y_pred.requires_grad == True
+        loss = self.loss(y_pred, y_true)
+        loss.backward()
+        self.optim.step()
+        
         self.last_phi_state = phi2
-        
     
-    def act(self, observation, reward, done):
-        self._update(observation, reward, done)
-        
-        self.c += 1
-        if self.c % self.C == 0 and self.with_target_network:
-            self.Q_old = copy.deepcopy(self.Q)
-        
+    def act(self):
         self.eps *= self.eps_decay
         if np.random.random() < self.eps:
             a = np.random.choice(self.action_space, 1)[0]
@@ -99,6 +91,17 @@ class DQN_Agent:
         
         self.last_action = a
         return a
+    
+    def get_result(self, observation, reward, done):
+        self._update(observation, reward, done)
+        
+        self.c += 1
+        if self.c % self.C == 0 and self.with_target_network:
+            self.Q_old = copy.deepcopy(self.Q)
+    
+    def reset(self, observation):
+        self.last_action = None
+        self.last_phi_state = self.phi(torch.tensor(observation))
 
 
 class NN(nn.Module):
@@ -109,6 +112,7 @@ class NN(nn.Module):
             self.layers.append(nn.Linear(inSize, x))
             inSize = x
         self.layers.append(nn.Linear(inSize, outSize))
+    
     def forward(self, x):
         x = self.layers[0](x.float())
         for i in range(1, len(self.layers)):
--- c_continuous_space/mainDQN.py
+++ c_continuous_space/mainDQN.py
@@ -2,7 +2,6 @@ import matplotlib
 
 matplotlib.use("TkAgg")
 import gym
-from gym import wrappers
 import numpy as np
 from DQN import NN, DQN_Agent
 from torch.optim import Adam
@@ -10,23 +9,25 @@ from time import time
 import torch.nn
 import matplotlib.pyplot as plt
 
+
 def identity(x):
     return x
 
-def loop(env, agent, outdir, render=False, feat_extractor=None):
-    envm = wrappers.Monitor(env, directory=outdir, force=True, video_callable=False)
+
+def loop(env, agent, render=False, feat_extractor=None):
     env.seed(0)
     
     episode_count = 1000
-    reward = 0
-    done = False
     env.verbose = True
     np.random.seed(5)
     lrsum = []
     t0 = time()
-
+    
     for i in range(episode_count):
-        obs = envm.reset()
+        obs = env.reset()
+        if feat_extractor:
+            obs = feat_extractor.getFeatures(obs)
+        agent.reset(obs)
         env.verbose = (i % 100 == 0 and i > 0)  # afficher 1 episode sur 100
         if env.verbose:
             print(i, "rsum %.2f" % np.mean(lrsum), "action_count", "time", time() - t0)
@@ -36,11 +37,12 @@ def loop(env, agent, outdir, render=False, feat_extractor=None):
         j = 0
         rsum = 0
         while True:
+            action = agent.act()
+            obs, reward, done, _ = env.step(action)
             if feat_extractor:
                 obs = feat_extractor.getFeatures(obs)
-
-            action = agent.act(obs, reward, done)
-            obs, reward, done, _ = envm.step(action)
+            agent.get_result(obs, reward, done)
+            
             rsum += reward
             j += 1
             if env.verbose and render:
@@ -49,122 +51,167 @@ def loop(env, agent, outdir, render=False, feat_extractor=None):
                 lrsum.append(rsum)
                 # print("Episode : " + str(i) + " rsum=" + str(rsum) + ", " + str(j) + " actions")
                 break
-
+    
     print("done")
     env.close()
     return lrsum
 
 
-def main_cartPole():
-    env = gym.make('CartPole-v1')
-    # Enregistrement de l'Agent
-
-    sizeIn = env.observation_space.shape[0]     #4  = len(phi(x)) #cart_pos, cart_spe, pole_pos, pos_spe
-    sizeOut = env.action_space.n                #[0, 1] # gauche droite
-    Q = NN(sizeIn, sizeOut, [24, 24])
+def main_gridWorld():
+    def getFeatures(obs):
+        """
+        prend en argument la carte du jeu originale et retourne un
+        vecteur de features contenant la position de l'agent, celles des éléments jaunes ainsi
+        que celles des éléments roses
+        """
+        state = np.zeros((3, np.shape(obs)[0], np.shape(obs)[1]))
+        state[0] = np.where(obs == 2, 1, state[0])
+        state[1] = np.where(obs == 4, 1, state[1])
+        state[2] = np.where(obs == 6, 1, state[2])
+        return state.flatten()
+    
+    env = gym.make("gridworld-v0")
+    sizeIn = 3 * 6 * 6
+    sizeOut = env.action_space.n
+    Q = NN(sizeIn, sizeOut, [24, 24])  #type: torch.nn.Module
     optim = Adam(Q.parameters(), lr=1e-3)
     agent = DQN_Agent(env, range(env.action_space.n),
-                       Q=Q,
-                       optim=optim,
-                       loss=torch.nn.MSELoss(),
-                       C=10, eps=1.0, eps_decay=.99, replay_memory_max_len=1000,
-                       replay_memory_n=100, gamma=1 - 1e-1, phi=identity)
-    agent_noExpReplay = DQN_Agent(env, range(env.action_space.n),
                       Q=Q,
                       optim=optim,
                       loss=torch.nn.MSELoss(),
                       C=10, eps=1.0, eps_decay=.99, replay_memory_max_len=1000,
-                      replay_memory_n=100, gamma=1 - 1e-1, phi=identity, with_exp_replay=False)
-    agent_noTargetNetwork = DQN_Agent(env, range(env.action_space.n),
+                      replay_memory_n=100, gamma=1 - 1e-1, phi=identity)
+    agent_noExpReplay = DQN_Agent(env, range(env.action_space.n),
                                   Q=Q,
                                   optim=optim,
                                   loss=torch.nn.MSELoss(),
                                   C=10, eps=1.0, eps_decay=.99, replay_memory_max_len=1000,
-                                  replay_memory_n=100, gamma=1 - 1e-1, phi=identity, with_target_network=False)
-    outdir = 'cartpole-v1/DQN-results'
-    outdir_noExpReplay = 'cartpole-v1/DQN-noExpReplay-results'
-    outdir_noTargetNetwork = 'cartpole-v1/DQN-noTargetNetwork-results'
+                                  replay_memory_n=100, gamma=1 - 1e-1, phi=identity, with_exp_replay=False)
+    agent_noTargetNetwork = DQN_Agent(env, range(env.action_space.n),
+                                      Q=Q,
+                                      optim=optim,
+                                      loss=torch.nn.MSELoss(),
+                                      C=10, eps=1.0, eps_decay=.99, replay_memory_max_len=1000,
+                                      replay_memory_n=100, gamma=1 - 1e-1, phi=identity, with_target_network=False)
+    
+    lrsum = loop(env, agent, render=False, feat_extractor=getFeatures)
+    lrsum_noExpReplay = loop(env, agent_noExpReplay, render=False, feat_extractor=getFeatures)
+    lrsum_noTargetNetwork = loop(env, agent_noTargetNetwork, render=False, feat_extractor=getFeatures)
+    
+    plt.plot(np.cumsum(lrsum), label="DQN", color="blue")
+    plt.plot(np.cumsum(lrsum_noExpReplay), label="DQN-noExpReplay", color="red")
+    plt.plot(np.cumsum(lrsum_noTargetNetwork), label="DQN-noTargetNetwork", color="green")
+    plt.xlabel("Episode")
+    plt.ylabel("Cumulative reward")
+    plt.legend()
+    plt.show()
 
-    lrsum = loop(env, agent, outdir, render=False)
-    lrsum_noExpReplay = loop(env, agent_noExpReplay, outdir_noExpReplay, render=False)
-    lrsum_noTargetNetwork = loop(env, agent_noTargetNetwork, outdir_noTargetNetwork, render=False)
 
+def main_cartPole():
+    env = gym.make('CartPole-v1')
+    # Enregistrement de l'Agent
+    
+    sizeIn = env.observation_space.shape[0]  #4  = len(phi(x)) #cart_pos, cart_spe, pole_pos, pos_spe
+    sizeOut = env.action_space.n  #[0, 1] # gauche droite
+    Q = NN(sizeIn, sizeOut, [24, 24])  #type: torch.nn.Module
+    optim = Adam(Q.parameters(), lr=1e-3)
+    agent = DQN_Agent(env, range(env.action_space.n),
+                      Q=Q,
+                      optim=optim,
+                      loss=torch.nn.MSELoss(),
+                      C=10, eps=1.0, eps_decay=.99, replay_memory_max_len=1000,
+                      replay_memory_n=100, gamma=1 - 1e-1, phi=identity)
+    agent_noExpReplay = DQN_Agent(env, range(env.action_space.n),
+                                  Q=Q,
+                                  optim=optim,
+                                  loss=torch.nn.MSELoss(),
+                                  C=10, eps=1.0, eps_decay=.99, replay_memory_max_len=1000,
+                                  replay_memory_n=100, gamma=1 - 1e-1, phi=identity, with_exp_replay=False)
+    agent_noTargetNetwork = DQN_Agent(env, range(env.action_space.n),
+                                      Q=Q,
+                                      optim=optim,
+                                      loss=torch.nn.MSELoss(),
+                                      C=10, eps=1.0, eps_decay=.99, replay_memory_max_len=1000,
+                                      replay_memory_n=100, gamma=1 - 1e-1, phi=identity, with_target_network=False)
+    
+    lrsum = loop(env, agent, render=False)
+    lrsum_noExpReplay = loop(env, agent_noExpReplay, render=False)
+    lrsum_noTargetNetwork = loop(env, agent_noTargetNetwork, render=False)
+    
     plt.plot(np.cumsum(lrsum), label="DQN", color="blue")
     plt.plot(np.cumsum(lrsum_noExpReplay), label="DQN-noExpReplay", color="red")
     plt.plot(np.cumsum(lrsum_noTargetNetwork), label="DQN-noTargetNetwork", color="green")
     plt.legend()
     plt.show()
 
+
 def main_acrobot():
     env = gym.make('Acrobot-v1')
     # Enregistrement de l'Agent
-
+    
     sizeIn = env.observation_space.shape[0]
     sizeOut = env.action_space.n
-    Q = NN(sizeIn, sizeOut, [])
+    Q = NN(sizeIn, sizeOut, [])  #type: torch.nn.Module
     optim = Adam(Q.parameters(), lr=1e-3)
     agent = DQN_Agent(env, range(env.action_space.n),
-                       Q=Q,
-                       optim=optim,
-                       loss=torch.nn.MSELoss(),
-                       C=10, eps=1.0, eps_decay=.99, replay_memory_max_len=1000,
-                       replay_memory_n=100, gamma=1 - 1e-1, phi=identity)
+                      Q=Q,
+                      optim=optim,
+                      loss=torch.nn.MSELoss(),
+                      C=10, eps=1.0, eps_decay=.99, replay_memory_max_len=1000,
+                      replay_memory_n=100, gamma=1 - 1e-1, phi=identity)
     
-    outdir = 'Acrobot-v1/random-agent-results'
-    loop(env, agent, outdir)
+    loop(env, agent)
+
 
 def main_mountainCar():
     env = gym.make('MountainCar-v0')
     # Enregistrement de l'Agent
-
+    
     sizeIn = env.observation_space.shape[0]
     sizeOut = env.action_space.n
-    Q = NN(sizeIn, sizeOut, [])
+    Q = NN(sizeIn, sizeOut, [])  #type: torch.nn.Module
     optim = Adam(Q.parameters(), lr=1e-3)
     agent = DQN_Agent(env, range(env.action_space.n),
-                       Q=Q,
-                       optim=optim,
-                       loss=torch.nn.MSELoss(),
-                       C=10, eps=1.0, eps_decay=.99, replay_memory_max_len=1000,
-                       replay_memory_n=100, gamma=1 - 1e-1, phi=identity)
+                      Q=Q,
+                      optim=optim,
+                      loss=torch.nn.MSELoss(),
+                      C=10, eps=1.0, eps_decay=.99, replay_memory_max_len=1000,
+                      replay_memory_n=100, gamma=1 - 1e-1, phi=identity)
+    
+    loop(env, agent)
 
-    outdir = 'MountainCar-v0/random-agent-results'
-    loop(env, agent, outdir)
 
 def main_lunarlander():
     env = gym.make('LunarLander-v2')
     # Enregistrement de l'Agent
-
+    
     sizeIn = env.observation_space.shape[0]
     sizeOut = env.action_space.n
-    Q = NN(sizeIn, sizeOut, [24, 24])
+    Q = NN(sizeIn, sizeOut, [24, 24])  #type: torch.nn.Module
     optim = Adam(Q.parameters(), lr=1e-3)
     agent = DQN_Agent(env, range(env.action_space.n),
-                       Q=Q,
-                       optim=optim,
-                       loss=torch.nn.MSELoss(),
-                       C=10, eps=1.0, eps_decay=.99, replay_memory_max_len=1000,
-                       replay_memory_n=100, gamma=1 - 1e-1, phi=identity)
+                      Q=Q,
+                      optim=optim,
+                      loss=torch.nn.MSELoss(),
+                      C=10, eps=1.0, eps_decay=.99, replay_memory_max_len=1000,
+                      replay_memory_n=100, gamma=1 - 1e-1, phi=identity)
     agent_noExpReplay = DQN_Agent(env, range(env.action_space.n),
-                      Q=Q,
-                      optim=optim,
-                      loss=torch.nn.MSELoss(),
-                      C=10, eps=1.0, eps_decay=.99, replay_memory_max_len=1000,
-                      replay_memory_n=100, gamma=1 - 1e-1, phi=identity, with_exp_replay=False)
+                                  Q=Q,
+                                  optim=optim,
+                                  loss=torch.nn.MSELoss(),
+                                  C=10, eps=1.0, eps_decay=.99, replay_memory_max_len=1000,
+                                  replay_memory_n=100, gamma=1 - 1e-1, phi=identity, with_exp_replay=False)
     agent_noTargetNetwork = DQN_Agent(env, range(env.action_space.n),
-                      Q=Q,
-                      optim=optim,
-                      loss=torch.nn.MSELoss(),
-                      C=10, eps=1.0, eps_decay=.99, replay_memory_max_len=1000,
-                      replay_memory_n=100, gamma=1 - 1e-1, phi=identity, with_target_network=False)
-    outdir = 'LunarLander-v2/DQN-results'
-    outdir_noExpReplay = 'LunarLander-v2/DQN-noExpReplay-results'
-    outdir_noTargetNetwork = 'LunarLander-v2/DQN-noTargetNetwork-results'
-
-    lrsum = loop(env, agent, outdir, render=False)
-    lrsum_noExpReplay = loop(env, agent_noExpReplay, outdir_noExpReplay, render=False)
-    lrsum_noTargetNetwork = loop(env, agent_noTargetNetwork, outdir_noTargetNetwork, render=False)
-
+                                      Q=Q,
+                                      optim=optim,
+                                      loss=torch.nn.MSELoss(),
+                                      C=10, eps=1.0, eps_decay=.99, replay_memory_max_len=1000,
+                                      replay_memory_n=100, gamma=1 - 1e-1, phi=identity, with_target_network=False)
+    
+    lrsum = loop(env, agent, render=False)
+    lrsum_noExpReplay = loop(env, agent_noExpReplay, render=False)
+    lrsum_noTargetNetwork = loop(env, agent_noTargetNetwork, render=False)
+    
     plt.plot(np.cumsum(lrsum), label="DQN", color="blue")
     plt.plot(np.cumsum(lrsum_noExpReplay), label="DQN-noExpReplay", color="red")
     plt.plot(np.cumsum(lrsum_noTargetNetwork), label="DQN-noTargetNetwork", color="green")
